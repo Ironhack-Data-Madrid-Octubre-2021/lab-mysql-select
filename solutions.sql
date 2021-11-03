df4daa95f7f6f08b7462eb11819c89d0f9fc812a
mariaperezdeayalas <mperezdeayalas@gmail.com>
2021-11-03 17:39:21 +0100

    lab mysql select hecho

---
+++ solutions.sql
@@ -0,0 +1,48 @@
+# CHALLENGE I
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, t.title AS TITLE, p.pub_name AS PUBLISHER
+FROM authors as a
+LEFT JOIN titleauthor as au
+ON a.au_id = au.au_id
+LEFT JOIN titles as t
+ON au.title_id = t.title_id
+LEFT JOIN publishers as p
+ON t.pub_id = p.pub_id;
+
+# CHALLENGE II
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, p.pub_name AS PUBLISHER, COUNT(t.title_id) AS TITLE_COUNT
+FROM authors as a
+LEFT JOIN titleauthor as au
+ON a.au_id = au.au_id
+LEFT JOIN titles as t
+ON au.title_id = t.title_id
+LEFT JOIN publishers as p
+ON t.pub_id = p.pub_id
+GROUP BY a.au_id, p.pub_name;
+
+# CHALLENGE III
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, SUM(s.qty) AS TOTAL_SALES
+FROM authors as a
+LEFT JOIN titleauthor as au
+ON a.au_id = au.au_id
+LEFT JOIN titles as t
+ON au.title_id = t.title_id
+LEFT JOIN sales AS s
+ON t.title_id = s.title_id
+GROUP BY a.au_id
+ORDER BY TOTAL_SALES DESC
+LIMIT 3;
+
+# CHALLENGE IV
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, IFNULL(SUM(s.qty), 0) AS TOTAL_SALES
+FROM authors as a
+LEFT JOIN titleauthor as au
+ON a.au_id = au.au_id
+LEFT JOIN titles as t
+ON au.title_id = t.title_id
+LEFT JOIN sales AS s
+ON t.title_id = s.title_id
+GROUP BY a.au_id
+ORDER BY TOTAL_SALES DESC
+LIMIT 23;
+
+
