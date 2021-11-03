3331ad271ab461e1d9249338cec9b29f586f6100
pedromartinezleis <60599980+pedromartinezleis@users.noreply.github.com>
2021-11-03 17:07:10 +0100

    Add files via upload

--- solution.sql
+++ solution.sql
@@ -11,18 +11,20 @@ WHERE a.au_id = b.au_id AND b.title_id = c.title_id AND c.pub_id = d.pub_id
 GROUP BY a.au_id;
 
 #####Query tres#####
-select a.au_id AS "AUTHOR ID", a.au_lname AS "LAST NAME", a.au_fname AS "FIRST NAME", count(title) AS "TOTAL"
-FROM authors a, titleauthor b, titles c
-WHERE a.au_id = b.au_id AND b.title_id = c.title_id
+select a.au_id AS "AUTHOR ID", a.au_lname AS "LAST NAME", a.au_fname AS "FIRST NAME", sum(d.qty) AS "TOTAL"
+FROM authors a, titleauthor b, titles c, sales d
+WHERE a.au_id = b.au_id AND b.title_id = c.title_id AND b.title_id = d.title_id
 GROUP BY a.au_id
 ORDER BY count(title) DESC
 LIMIT 3;
 
 #####Query cuatro#####
-select a.au_id AS "AUTHOR ID", a.au_lname AS "LAST NAME", a.au_fname AS "FIRST NAME", count(title) AS "TOTAL"
+select a.au_id AS "AUTHOR ID", a.au_lname AS "LAST NAME", a.au_fname AS "FIRST NAME", ifnull(sum(d.qty),0) AS "TOTAL"
 FROM authors a LEFT JOIN titleauthor b
 ON a.au_id = b.au_id 
 LEFT JOIN titles c
 ON b.title_id = c.title_id
+LEFT JOIN sales d
+ON b.title_id = d.title_id
 GROUP BY a.au_id
-ORDER BY count(title) DESC;
+ORDER BY sum(d.qty) DESC;
